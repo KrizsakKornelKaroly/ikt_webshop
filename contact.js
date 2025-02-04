eeacda7204d69cd1bbabc64a7201ecee5971d0b3
Zalan12 <farkaszalan@turr.hu>
2025-02-04 22:23:27 +0100

    WS-35 -JS alapok kialakítása, adattárolás

--- contact.js
+++ contact.js
@@ -1,4 +1,18 @@
+let adatok=[];
 function Adatfelvetel()
 {
-    alert("Belo");
+    let adat={vnev:document.getElementById("veznev").value,
+            kernev:document.getElementById("kernev").value,
+            telo:document.getElementById("telefon").value,
+            email:document.getElementById("email").value,
+            orszag:document.getElementById("orszagok").value,
+            regio:document.getElementById("regio").value,
+            nem:document.getElementById("nem").value,
+            megjegyzes:document.getElementById("megjegyzes").value
+
+    }
+    adatok.push(adat);
+    alert("Adatok sikeresen felvéve. Köszönjük a visszajelzésed! \nNév: "+adatok[0].vnev);
+        
+    
 }
