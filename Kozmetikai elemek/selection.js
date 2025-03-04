f5027f2469f2b9d8d60eed3d2c73123e87c75b70
Zalan12 <farkaszalan@turr.hu>
2025-03-04 10:33:35 +0100

    WS-46 -Adatfeltöltés kijavítása, univerzálissá tétele

--- Kozmetikai elemek/selection.js
+++ Kozmetikai elemek/selection.js
@@ -37,37 +37,73 @@ let tomb=[
     },
     adat7={
         ar:"25$",
-        id:"N",
+        id:"F",
         img:"./Kepek/feherFelsoF.jpg",
         nev:"Fehér férfi felső"
     },
     adat8={
         ar:"120$",
-        id:"N",
+        id:"F",
         img:"./Kepek/feherKabatF.jpg",
         nev:"Fehér férfi kabát"
     },
     adat9={
         ar:"30$",
-        id:"N",
+        id:"F",
         img:"./Kepek/feketefelsoF.jpg",
         nev:"Fekete férfi felső"
     },
     adat10={
         ar:"45$",
-        id:"N",
+        id:"F",
         img:"./Kepek/kekfelsoF.jpg",
         nev:"Kék férfi felső"
     },
     adat11={
         ar:"500$",
-        id:"N",
-        img:"./Kepek/leatherJacketF.jpg",
+        id:"F",
+        img:"./Kepek/leatherJacketF.jpeg",
         nev:"Fekete bőrkabát"
     },
     adat12={
         ar:"25$",
-        id:"N",
+        id:"F",
+        img:"./Kepek/szurkeoltonyF.jpg",
+        nev:"Szürke férfi öltöny"
+    },
+    adat13={
+        ar:"25$",
+        id:"F",
+        img:"./Kepek/feherFelsoF.jpg",
+        nev:"Fehér férfi felső"
+    },
+    adat14={
+        ar:"120$",
+        id:"F",
+        img:"./Kepek/feherKabatF.jpg",
+        nev:"Fehér férfi kabát"
+    },
+    adat15={
+        ar:"30$",
+        id:"F",
+        img:"./Kepek/feketefelsoF.jpg",
+        nev:"Fekete férfi felső"
+    },
+    adat16={
+        ar:"45$",
+        id:"F",
+        img:"./Kepek/kekfelsoF.jpg",
+        nev:"Kék férfi felső"
+    },
+    adat17={
+        ar:"500$",
+        id:"F",
+        img:"./Kepek/leatherJacketF.jpeg",
+        nev:"Fekete bőrkabát"
+    },
+    adat18={
+        ar:"25$",
+        id:"F",
         img:"./Kepek/szurkeoltonyF.jpg",
         nev:"Szürke férfi öltöny"
     }
@@ -76,15 +112,81 @@ function NoiAdatok()
 {
     for(let i=0;i<tomb.length;i++)
     {
-        let kep = document.querySelectorAll('.kartyaKep')[i];
-        kep.src=tomb[i].img;
-        kep.setAttribute("title",tomb[i].nev)
-        let termAr=document.querySelectorAll('.price')[i];
-        termAr.innerHTML=tomb[i].ar;
-        let titleK=document.querySelectorAll('.title')[i];
-        titleK.textContent="";
-        titleK.innerHTML=tomb[i].nev;
+        if(tomb[i].id=="N")
+        {
+            let kep = document.querySelectorAll('.kartyaKep')[i];
+            kep.src=tomb[i].img;
+            kep.title=tomb[i].nev;
+            kep.setAttribute("title",tomb[i].nev)
+            let termAr=document.querySelectorAll('.price')[i];
+            termAr.innerHTML=tomb[i].ar;
+            let titleK=document.querySelectorAll('.title')[i];
+            titleK.textContent="";
+            titleK.innerHTML=tomb[i].nev;
+        }
+
 
     }
 }
-NoiAdatok();
+
+function FerfiAdatok()
+{
+    let j=0;
+    for(let i=0;i<tomb.length;i++)
+    {
+        if(tomb[i].id=="F")
+        {
+            
+            let kep = document.querySelectorAll('.kartyaKep')[j];
+            kep.src=tomb[i].img;
+            kep.title=tomb[i].nev;
+            kep.setAttribute("title",tomb[i].nev)
+            let termAr=document.querySelectorAll('.price')[j];
+            termAr.innerHTML=tomb[i].ar;
+            let titleK=document.querySelectorAll('.title')[j];
+            titleK.textContent="";
+            titleK.innerHTML=tomb[i].nev;
+            j++;
+        }
+
+
+    }
+}
+
+function KiegAdatok()
+{
+    let j=0;
+    for(let i=0;i<tomb.length;i++)
+    {
+        if(tomb[i].id=="")
+        {
+            
+            let kep = document.querySelectorAll('.kartyaKep')[j];
+            kep.src=tomb[i].img;
+            kep.title=tomb[i].nev;
+            kep.setAttribute("title",tomb[i].nev)
+            let termAr=document.querySelectorAll('.price')[j];
+            termAr.innerHTML=tomb[i].ar;
+            let titleK=document.querySelectorAll('.title')[j];
+            titleK.textContent="";
+            titleK.innerHTML=tomb[i].nev;
+            j++;
+        }
+
+
+    }
+}
+
+if(document.title=="Női ruhák")
+{
+    NoiAdatok();
+}
+
+if(document.title=="Férfi ruhák")
+{
+    FerfiAdatok();
+}
+if(document.title=="Kiegészítők")
+{
+    KiegAdatok();
+}
