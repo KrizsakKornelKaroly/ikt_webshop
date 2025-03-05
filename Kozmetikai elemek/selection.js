e159198eee3a2835eeb3b7df2cb8ddc1a84910f4
Zalan12 <farkaszalan@turr.hu>
2025-03-05 08:39:56 +0100

    WS-46 -Adatfeltöltés folytatása a clothing oldalon, linkelési problémák javítása

--- Kozmetikai elemek/selection.js
+++ Kozmetikai elemek/selection.js
@@ -1,112 +1,150 @@
 let tomb=[
      adat1={
-        ar:"20$",
+        ar:"20",
         id:"N",
         img:"./Kepek/lilaPoloN.jpg",
         nev:"Lila Póló"
     },
     adat2={
-        ar:"200$",
+        ar:"200",
         id:"N",
         img:"./Kepek/szormeKabatN.jpg",
         nev:"Szőrme kabát"
     },
     adat3={
-        ar:"10$",
+        ar:"10",
         id:"N",
         img:"./Kepek/feketePoloN.jpg",
         nev:"Fekete Női Póló"
     },
     adat4={
-        ar:"45$",
+        ar:"45",
         id:"N",
         img:"./Kepek/feketeTaskaN.jpg",
         nev:"Fekete női táska"
     },
     adat5={
-        ar:"1000$",
+        ar:"1000",
         id:"N",
         img:"./Kepek/nyaklancN.jpg",
         nev:"Női gyémánt nyaklánc"
     },
     adat6={
-        ar:"25$",
+        ar:"25",
         id:"N",
         img:"./Kepek/nyariRuhaN.jpg",
         nev:"Női nyári ruha"
     },
     adat7={
-        ar:"25$",
+        ar:"25",
         id:"F",
         img:"./Kepek/feherFelsoF.jpg",
         nev:"Fehér férfi felső"
     },
     adat8={
-        ar:"120$",
+        ar:"120",
         id:"F",
         img:"./Kepek/feherKabatF.jpg",
         nev:"Fehér férfi kabát"
     },
     adat9={
-        ar:"30$",
+        ar:"30",
         id:"F",
         img:"./Kepek/feketefelsoF.jpg",
         nev:"Fekete férfi felső"
     },
     adat10={
-        ar:"45$",
+        ar:"45",
         id:"F",
         img:"./Kepek/kekfelsoF.jpg",
         nev:"Kék férfi felső"
     },
     adat11={
-        ar:"500$",
+        ar:"500",
         id:"F",
         img:"./Kepek/leatherJacketF.jpeg",
         nev:"Fekete bőrkabát"
     },
     adat12={
-        ar:"25$",
+        ar:"25",
         id:"F",
         img:"./Kepek/szurkeoltonyF.jpg",
         nev:"Szürke férfi öltöny"
     },
     adat13={
-        ar:"45$",
+        ar:"45",
         id:"K",
         img:"./Kepek/barnaTaskaK.jpg",
         nev:"Barna férfi hátitáska"
     },
     adat14={
-        ar:"12$",
+        ar:"12",
         id:"K",
         img:"./Kepek/borosUvegK.jpg",
         nev:"Fekete üvegpalack"
     },
     adat15={
-        ar:"10$",
+        ar:"10",
         id:"K",
         img:"./Kepek/keresztnyaklK.jpg",
         nev:"Kereszt nyaklánc"
     },
     adat16={
-        ar:"45$",
+        ar:"45",
         id:"K",
         img:"./Kepek/rozsaTaskaK.jpg",
         nev:"Rózsaszín hátitáska"
     },
     adat17={
-        ar:"12$",
+        ar:"12",
         id:"K",
         img:"./Kepek/rozsaUvegK.jpg",
         nev:"Rózsaszín üvegpalack"
     },
     adat18={
-        ar:"25$",
+        ar:"25",
         id:"K",
         img:"./Kepek/sportTaskaK.jpg",
         nev:"Férfi sporttáska"
+    },
+    adat19={
+        ar:"70",
+        id:"R",
+        img:"./Kepek/barnaBorcipoR.jpg",
+        nev:"Bana férfi bőrcipő"
+    },
+    adat20={
+        ar:"60",
+        id:"R",
+        img:"./Kepek/feketeCipoR.jpg",
+        nev:"Fekete női cipő"
+    },
+    adat21={
+        ar:"90",
+        id:"R",
+        img:"./Kepek/feketecsizmaR.jpg",
+        nev:"Fekete Férfi Csizma"
+    },
+    adat22={
+        ar:"15",
+        id:"R",
+        img:"./Kepek/feketeKesztyuR.jpg",
+        nev:"Fekete téli kesztyű"
+    },
+    adat23={
+        ar:"100",
+        id:"R",
+        img:"./Kepek/feketeNőiCsizmaR.jpg",
+        nev:"Fekete Női csizma"
+    },
+    adat24={
+        ar:"75",
+        id:"R",
+        img:"./Kepek/keksportcipoR.jpg",
+        nev:"Kék sportcipő"
     }
+    
+
 ];
 function NoiAdatok()
 {
@@ -119,7 +157,7 @@ function NoiAdatok()
             kep.title=tomb[i].nev;
             kep.setAttribute("title",tomb[i].nev)
             let termAr=document.querySelectorAll('.price')[i];
-            termAr.innerHTML=tomb[i].ar;
+            termAr.innerHTML="$"+tomb[i].ar;
             let titleK=document.querySelectorAll('.title')[i];
             titleK.textContent="";
             titleK.innerHTML=tomb[i].nev;
@@ -142,7 +180,7 @@ function FerfiAdatok()
             kep.title=tomb[i].nev;
             kep.setAttribute("title",tomb[i].nev)
             let termAr=document.querySelectorAll('.price')[j];
-            termAr.innerHTML=tomb[i].ar;
+            termAr.innerHTML="$"+tomb[i].ar;
             let titleK=document.querySelectorAll('.title')[j];
             titleK.textContent="";
             titleK.innerHTML=tomb[i].nev;
@@ -166,7 +204,31 @@ function KiegAdatok()
             kep.title=tomb[i].nev;
             kep.setAttribute("title",tomb[i].nev)
             let termAr=document.querySelectorAll('.price')[j];
-            termAr.innerHTML=tomb[i].ar;
+            termAr.innerHTML="$"+tomb[i].ar;
+            let titleK=document.querySelectorAll('.title')[j];
+            titleK.textContent="";
+            titleK.innerHTML=tomb[i].nev;
+            j++;
+        }
+
+
+    }
+}
+
+function RuhaAdatok()
+{
+    let j=0;
+    for(let i=0;i<tomb.length;i++)
+    {
+        if(tomb[i].id=="R")
+        {
+            
+            let kep = document.querySelectorAll('.kartyaKep')[j];
+            kep.src=tomb[i].img;
+            kep.title=tomb[i].nev;
+            kep.setAttribute("title",tomb[i].nev)
+            let termAr=document.querySelectorAll('.price')[j];
+            termAr.innerHTML="$"+tomb[i].ar;
             let titleK=document.querySelectorAll('.title')[j];
             titleK.textContent="";
             titleK.innerHTML=tomb[i].nev;
@@ -189,4 +251,8 @@ if(document.title=="Férfi ruhák")
 if(document.title=="Kiegészítők")
 {
     KiegAdatok();
+}
+if(document.title=="Egyéb ruházatok, cipők")
+{
+    RuhaAdatok();
 }
