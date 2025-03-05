5959cd852acddff6acc93b15e2cb9a2e6dd4fb1c
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-03-05 08:40:16 +0100

    WS-38 - Nav script-reszponzivitás hiba javítása

--- Kozmetikai elemek/navOpen.js
+++ Kozmetikai elemek/navOpen.js
@@ -1,40 +1,22 @@
-let alapszel = window.innerWidth;
+let nyitoGomb = document.getElementById("menuOpen")
+let teljesMenu = document.getElementsByClassName("navJobb")[0]
+
 function Nyitas() {
-    menunyitocska = document.getElementsByClassName("navJobb")[0]
-    menunyitocska.style.display = "block"
-    console.log(window.innerWidth)
-    if (szelAdat <= 820) {
-        if (menunyitocska.style.display == "none") {
-            menunyitocska.style.display = "block"
-        }
-        else (
-            menunyitocska.style.display = "none"
-        )
-    }
-    else {
-        menunyitocska.style.display = "block"
-    }
-}
-function handleResize() {
-    console.log("változás")
-    let mostaniSzel = window.innerWidth;
-    if (alapszel !== mostaniSzel) {
-        alapszel = mostaniSzel;
-        console.log(window.innerWidth)
-        // Your function to run when the width changes
-        if (window.innerWidth <= 820) {
-            if (menunyitocska.style.display == "none") {
-                menunyitocska.style.display = "block"
-            }
-            else (
-                menunyitocska.style.display = "none"
-            )
+    if (window.innerWidth <= 820) {
+        if (teljesMenu.style.display == "none") {
+            teljesMenu.style.display = "block"
         }
         else {
-            menunyitocska.style.display = "block"
+            teljesMenu.style.display = "none"
         }
     }
 }
 
-// Listen to the resize event
-window.addEventListener('resize', console.log(window.innerWidth));
+setInterval(() => {
+    if (window.innerWidth > 820) {
+        if (teljesMenu.style.display == "none") {
+            teljesMenu.style.display = "block"
+        }
+    }
+}, 10);
+
