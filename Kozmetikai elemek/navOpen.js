2e1067d5c197f6ec23e886e625c7c2ef8f6666c9
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-03-25 10:31:21 +0100

    WS-38 - Mobil nézetes lenyíló menü elkészítése

--- Kozmetikai elemek/navOpen.js
+++ Kozmetikai elemek/navOpen.js
@@ -1,5 +1,6 @@
 let nyitoGomb = document.getElementById("menuOpen")
 let teljesMenu = document.getElementsByClassName("navJobb")[0]
+let ruhaKategoriak = document.getElementsByClassName("shopDDtartalom")[0]
 
 function Nyitas() {
     if (window.innerWidth <= 820) {
@@ -12,14 +13,26 @@ function Nyitas() {
     }
 }
 
+function mobileShopOpen(){
+    if (window.innerWidth <= 820) {
+        if (ruhaKategoriak.style.display == "none") {
+            ruhaKategoriak.style.display = "block"
+        }
+        else {
+            ruhaKategoriak.style.display = "none"
+        }
+    }
+}
+
+
 setInterval(() => {
     if (window.innerWidth > 820) {
         if (teljesMenu.style.display == "none") {
             teljesMenu.style.display = "block"
         }
+        /*
+        if (ruhaKategoriak.style.display == "none") {
+            ruhaKategoriak.style.display = "block"
+        }*/
     }
 }, 10);
-
-function mobileShopOpen(){
-    alert("Ezjó")
-}
