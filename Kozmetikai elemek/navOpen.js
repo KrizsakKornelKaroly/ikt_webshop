f2d481ebb3f5113bf5dfe4279a4d4b5ff6f4492e
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-03-26 08:43:28 +0100

    WS-38 - Dropdown megjelenési hiba javításának próbája, félsiker

--- Kozmetikai elemek/navOpen.js
+++ Kozmetikai elemek/navOpen.js
@@ -1,6 +1,7 @@
 let nyitoGomb = document.getElementById("menuOpen")
 let teljesMenu = document.getElementsByClassName("navJobb")[0]
 let ruhaKategoriak = document.getElementsByClassName("shopDDtartalom")[0]
+let ruhaGomb = document.getElementsByClassName("shopDDgomb")[0]
 
 function Nyitas() {
     if (window.innerWidth <= 820) {
@@ -24,15 +25,34 @@ function mobileShopOpen(){
     }
 }
 
-
 setInterval(() => {
     if (window.innerWidth > 820) {
         if (teljesMenu.style.display == "none") {
             teljesMenu.style.display = "block"
         }
+
         /*
-        if (ruhaKategoriak.style.display == "none") {
-            ruhaKategoriak.style.display = "block"
-        }*/
+        ruhaGomb.addEventListener('mouseenter', () => {
+            ruhaKategoriak.style.display = "block"; 
+        });
+        ruhaGomb.addEventListener('mouseleave', () => {
+            ruhaKategoriak.style.display = "none"; 
+        });
+        */
+
+        ruhaGomb.addEventListener('mouseenter', hoverTesting)
+        ruhaGomb.addEventListener('mouseleave', hoverTesting)
+        
     }
 }, 10);
+
+function hoverTesting(){
+    if(ruhaKategoriak.style.display == "block"){
+        ruhaKategoriak.style.display = "none"; 
+    }
+    else{
+        ruhaKategoriak.style.display = "block";
+    }
+}
+
+
