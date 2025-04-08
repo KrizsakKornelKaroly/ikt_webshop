e91614bffb063eb968520400b16f1de9671e6e9b
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-04-08 10:26:49 +0200

    WS-59 - Blog reszponzivitás + bejegyzéstároló létrehozása

---
+++ Kozmetikai elemek/blogContent.js
@@ -0,0 +1,12 @@
+let blogbejegyzesek = 
+[
+    {
+        cim: "Tejszínhabos lókolbász",
+        shortContent: "Lorem ipse dolor sit amet illumine patris és társai kft részvénytársaság kolbász paprika",
+        kepeleres: "./Kepek/taskaBlog.jpg",
+        longContent: {
+            bejegyzCim: "Az új tejszínhabos lókolbász megjelentetett"
+            
+        }
+    }
+]
