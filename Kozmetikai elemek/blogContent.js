aaae7f78a72b7d1b7f9e4b4815d74db2460bf40a
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2025-05-19 16:59:50 +0200

    WS-59 - Teljes újrafejlesztett Blog oldal v1.0

--- Kozmetikai elemek/blogContent.js
+++ Kozmetikai elemek/blogContent.js
@@ -1,54 +1,129 @@
 let blogbejegyzesek = 
 [
     {
-        cim: "Tejszínhabos lókolbász",
+        cim: "Új bőrtáska",
         ar: "25.99",
-        shortContent: "Lorem ipse dolor sit amet illumine patris és társai kft részvénytársaság kolbász paprika",
+        shortContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta doloribus fuga, tempora obcaecati labore aliquam commodi possimus fugit accusamus eaque itaque perferendis laboriosam iste! Saepe corrupti enim quis magnam rerum?",
         kepeleres: "./Kepek/taskaBlog.jpg",
         longContent: {
-            bejegyzCim: "Az új tejszínhabos lókolbász megjelentetett",
-            tempSzov: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis vel"
+            bejegyzCim: "Új bőrtárkák webshopunkban",
+            tempSzov: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor."   
+        }
+    },
+    {
+        cim: "Új bőrkabátok",
+        ar: "99.99",
+        shortContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta doloribus fuga, tempora obcaecati labore aliquam commodi possimus fugit accusamus eaque itaque perferendis laboriosam iste! Saepe corrupti enim quis magnam rerum?",
+        kepeleres: "./Kepek/bacsiBlog.jpg",
+        longContent: {
+            bejegyzCim: "Megérkeztek az új bőrkabátok",
+            tempSzov: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor."   
+        }
+    },
+    {
+        cim: "Új nyári termékeink",
+        ar: "15 - $89",
+        shortContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta doloribus fuga, tempora obcaecati labore aliquam commodi possimus fugit accusamus eaque itaque perferendis laboriosam iste! Saepe corrupti enim quis magnam rerum?",
+        kepeleres: "./Kepek/modernNoBlog.jpg",
+        longContent: {
+            bejegyzCim: "Újdonsült nyári férfi és női termékeink",
+            tempSzov: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor."   
+        }
+    },
+    {
+        cim: "Köldökszösz táska akció",
+        ar: "49.99",
+        shortContent: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta doloribus fuga, tempora obcaecati labore aliquam commodi possimus fugit accusamus eaque itaque perferendis laboriosam iste! Saepe corrupti enim quis magnam rerum?",
+        kepeleres: "./Kepek/modernNoBlog.jpg",
+        longContent: {
+            bejegyzCim: "Táska kiárusítás csak nálunk",
+            tempSzov: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare pharetra turpis, non efficitur massa elementum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in porttitor erat. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor. Morbi vitae ligula eu quam accumsan ullamcorper in at mauris. Donec nec convallis velit. Suspendisse faucibus nisl non est sagittis, pretium luctus est ullamcorper. Vivamus sed orci id est porta laoreet vel vel quam. Nulla pellentesque gravida nibh et auctor."   
+        }
+    }
+
+    
+]
+
+let blogLoad = false;
+window.onload = function () {
+    BejegyzesekGenerator(blogbejegyzesek, true);
+}
+
+function BejegyzesekGenerator(tomb, betoltes)
+{
+    if(betoltes)
+    {
+        for (let i = 0; i < tomb.length; i++) 
+        {
+            let bejegyzesKartya = document.createElement("div")
+            bejegyzesKartya.classList.add("bejegyzes")
+
+            let bejegyzesKep = document.createElement("img")
+            bejegyzesKep.src = blogbejegyzesek[i].kepeleres
+            bejegyzesKep.classList.add("blogkep")
+
+            let bejegyzesRovidSzovParent = document.createElement("div")
+            bejegyzesRovidSzovParent.classList.add("blogszoveg")
+
+            let bejegyzesCim = document.createElement("h3")
+            bejegyzesCim.textContent = blogbejegyzesek[i].cim
+
+            let bejegyzesRovidSzov = document.createElement("p")
+            bejegyzesRovidSzov.textContent = blogbejegyzesek[i].shortContent
+
+            let bejegyzesTovabb = document.createElement("button")
+            bejegyzesTovabb.classList.add("blogOpenBtn")
+            bejegyzesTovabb.textContent = "Tovább"
+            bejegyzesTovabb.setAttribute('onclick', `OpenPopup(${i})`)
+
+            bejegyzesRovidSzovParent.appendChild(bejegyzesCim)
+            bejegyzesRovidSzovParent.appendChild(bejegyzesRovidSzov)
+            bejegyzesRovidSzovParent.appendChild(bejegyzesTovabb)
+
+            bejegyzesKartya.appendChild(bejegyzesKep)
+            bejegyzesKartya.appendChild(bejegyzesRovidSzovParent)
+
+            document.getElementById("blogresz").appendChild(bejegyzesKartya)
+
             
         }
     }
-]
+}
 
-function OpenTest(){
+function OpenPopup(elemSorszam)
+{
     let popupMain = document.createElement("div")
-
     let leftSide = document.createElement("div")
     let rightSide = document.createElement("div")
     let popupBg = document.createElement("div")
     let popupContent = document.createElement("div")
     let popupClose = document.createElement("button")
 
-
     leftSide.classList.add("popupBaloldal")
     let kepLeft = document.createElement("img") 
     kepLeft.classList.add("popupKepLeft")
-    kepLeft.src = blogbejegyzesek[0].kepeleres
+    kepLeft.src = blogbejegyzesek[elemSorszam].kepeleres
 
     let termekLeft = document.createElement("h3")
     termekLeft.classList.add("popupLeftTermekCim")
-    termekLeft.textContent = blogbejegyzesek[0].cim
+    termekLeft.textContent = blogbejegyzesek[elemSorszam].cim
 
     let arLeft = document.createElement("h3")
     arLeft.classList.add("popupLeftAr")
-    arLeft.textContent = `$${blogbejegyzesek[0].ar}`
+    arLeft.textContent = `$${blogbejegyzesek[elemSorszam].ar}`
 
     let atcLeft = document.createElement("button")
-    atcLeft.textContent = "Add to cart"
+    atcLeft.textContent = "Megtekintés"
     atcLeft.classList.add("atcGombLeft")
 
     rightSide.classList.add("popupJobboldal")
     let cimJobb = document.createElement("h2")
-    cimJobb.textContent = blogbejegyzesek[0].longContent.bejegyzCim
+    cimJobb.textContent = blogbejegyzesek[elemSorszam].longContent.bejegyzCim
     cimJobb.classList.add("popupCimRight")
 
     let szovegJobb = document.createElement("div")
-    szovegJobb.textContent = blogbejegyzesek[0].longContent.tempSzov
+    szovegJobb.textContent = blogbejegyzesek[elemSorszam].longContent.tempSzov
     szovegJobb.classList.add("popupSzovegRight")
-
 
     popupClose.onclick = function()
     {
@@ -56,8 +131,10 @@ function OpenTest(){
     }
     popupClose.textContent = "X"
 
-
-    /*popupMain.classList.add("popupBg")*/
+    popupBg.onclick = function()
+    {
+        popupMain.remove();
+    }
     
     popupBg.classList.add("popupBgOpaque")
     popupContent.classList.add("popupCont")
@@ -66,7 +143,7 @@ function OpenTest(){
     popupBg.textContent = " "
     popupMain.appendChild(popupBg)
     popupMain.appendChild(popupContent)
-    popupContent.appendChild(popupClose)
+    popupBg.appendChild(popupClose)
     document.body.appendChild(popupMain)
     popupContent.appendChild(leftSide)
     leftSide.appendChild(kepLeft)
